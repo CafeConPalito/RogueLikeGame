d3309c5f5e3f9f2a7efa03b4e2c7f244322d7b0c
Daniel-Espinosa <anareldis@gmail.com>
2023-10-29 13:24:00 +0100

    Dice

--- RogueLikeGame/Assets/Scripts/Dice/Dice.cs
+++ RogueLikeGame/Assets/Scripts/Dice/Dice.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
 
@@ -14,4 +16,54 @@ public class Dice : MonoBehaviour
 
     private bool _hasStoppedRoll;
     private bool _delayFinished;
+
+    public static UnityAction<int, int> OnDiceResult;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    //https://www.youtube.com/watch?v=cd66wLNvVh8
+    private void Update()
+    {
+        if (!_delayFinished) return;
+
+        if (!_hasStoppedRoll && rb.velocity.sqrMagnitude == 0f)
+        {
+            _hasStoppedRoll = true;
+            GetFaceSideUp();
+        }
+    }
+    
+
+    
+    [ContextMenu("Que cara esta arriba")]
+    private int GetFaceSideUp()
+    {
+        if (diceFaces != null) return -1;
+
+        var topFace = 0;
+        var lastYPosition = diceFaces[0].position.y;
+
+        // por cada cara del dado comprobamos cual esta mas arriba
+        for (int i = 0; i < diceFaces.Length; i++)
+        {
+            if (diceFaces[i].position.y > lastYPosition) {
+
+                lastYPosition = diceFaces[i].position.y;
+                topFace = i;
+
+            }
+        }
+
+        Debug.Log( $"Dice result {topFace}");
+
+        OnDiceResult?.Invoke(_diceIndex, topFace);
+
+        return topFace;
+
+    }
+    
 }
