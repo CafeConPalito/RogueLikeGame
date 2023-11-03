7fa49ee98bfafd15594d53d670814775abe1d43d
Daniel-Espinosa <anareldis@gmail.com>
2023-11-03 18:05:42 +0100

    Dice, Arreglitos

--- RogueLikeGame/Assets/Scripts/DungeonGenerator/InitializeElements.cs
+++ RogueLikeGame/Assets/Scripts/DungeonGenerator/InitializeElements.cs
@@ -30,7 +30,7 @@ public class InitializeElements : MonoBehaviour
 
         spawnPoint = transform.TransformPoint(DungeonGenerator.roomsListToOperate[DungeonGenerator.roomsListToOperate.Length - 1].transform.position);
         Instantiate(bigBoss, spawnPoint, Quaternion.identity);
-
+        
     }
 
     // Start is called before the first frame update
